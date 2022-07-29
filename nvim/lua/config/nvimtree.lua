af14b6a4a46b74ac146db6321985641605446475
KS <leekashing2107@gmail.com>
2022-07-29 13:18:59 +0800

    remove not used options in nvim tree

--- nvim/lua/config/nvimtree.lua
+++ nvim/lua/config/nvimtree.lua
@@ -3,156 +3,51 @@ local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
 -- following options are the default
 tree.setup {
-  -- disables netrw completely
-  disable_netrw       = true,
-  -- hijack netrw window on startup
-  hijack_netrw        = true,
-  -- open the tree when running this setup function
-  open_on_setup       = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = false,
-  -- show lsp diagnostics in the signcolumn
-  diagnostics         = {
-    enable = false,
-  },
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable      = false,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {}
-  },
-  -- configuration options for the system open command (`s` in the tree by default)
-  system_open = {
-    -- the command to run this, leaving nil should work in most cases
-    cmd  = nil,
-    -- the command arguments as a list
-    args = {}
-  },
-
-  view = {
-    -- width of the window, can be either a number (columns) or a string in `%`
-    width = 40,
-    side = 'right',
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {
-
-        { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
-        { key = {"<2-RightMouse>", "c"},        cb = tree_cb("cd") },
-        { key = "v",                            cb = tree_cb("vsplit") },
-        { key = "V",                            cb = tree_cb("split") },
-        { key = "t",                            cb = tree_cb("tabnew") },
-        { key = "<",                            cb = tree_cb("prev_sibling") },
-        { key = ">",                            cb = tree_cb("next_sibling") },
-        { key = "P",                            cb = tree_cb("parent_node") },
-        { key = "<BS>",                         cb = tree_cb("close_node") },
-        { key = "<S-CR>",                       cb = tree_cb("close_node") },
-        { key = "<Tab>",                        cb = tree_cb("preview") },
-        { key = "K",                            cb = tree_cb("first_sibling") },
-        { key = "J",                            cb = tree_cb("last_sibling") },
-        { key = "I",                            cb = tree_cb("toggle_ignored") },
-        { key = "H",                            cb = tree_cb("toggle_dotfiles") },
-        { key = "R",                            cb = tree_cb("refresh") },
-        { key = "a",                            cb = tree_cb("create") },
-        { key = "d",                            cb = tree_cb("remove") },
-        { key = "r",                            cb = tree_cb("rename") },
-        { key = "<C-r>",                        cb = tree_cb("full_rename") },
-        { key = "x",                            cb = tree_cb("cut") },
-        { key = "yy",                           cb = tree_cb("copy") },
-        { key = "p",                            cb = tree_cb("paste") },
-        { key = "y",                            cb = tree_cb("copy_name") },
-        { key = "Y",                            cb = tree_cb("copy_path") },
-        { key = "gy",                           cb = tree_cb("copy_absolute_path") },
-        { key = "[c",                           cb = tree_cb("prev_git_item") },
-        { key = "]c",                           cb = tree_cb("next_git_item") },
-        { key = "b",                            cb = tree_cb("dir_up") },
-        { key = "s",                            cb = tree_cb("system_open") },
-        { key = "q",                            cb = tree_cb("close") },
-        { key = "g?",                           cb = tree_cb("toggle_help") },
-      }
-    }
-  }
-}
-
-
-vim.g.nvim_tree_git_hl = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g.nvim_tree_highlight_opened_files = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
-vim.g.nvim_tree_root_folder_modifier = ':~' --This is the default. See :help filename-modifiers for more options
-vim.g.nvim_tree_icon_padding = ' ' --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-vim.g.nvim_tree_window_picker_exclude = {
-    filetype = {
-       'packer',
-       'qf'
-    },
-    buftype = {
-       'terminal'
-    }
-}
--- Dictionary of buffer option names mapped to a list of option values that
--- indicates to the window picker that the buffer's window should not be
--- selectable.
-vim.g.nvim_tree_special_files = {
-    ["Cargo.toml"] = true,
-    Makefile = true,
-    ["README.md"] = true,
-    ["readme.md"] = true,
-    ["Makefile"] = true,
-} -- List of filenames that gets highlighted with NvimTreeSpecialFile
-
-vim.gnvim_tree_show_icons = {
-    git = true,
-    folders = true,
-    files = true,
-    folder_arrows = true,
-}
---If 0, do not show the icons for one of 'git' 'folder' and 'files'
---1 by default, notice that if 'files' is 1, it will only display
---if nvim-web-devicons is installed and on your runtimepath.
---if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
---but this will not work when you set indent_markers (because of UI conflict)
-
--- default will show icon by default if no icon is provided
--- default shows no icon by default
-vim.g.nvim_tree_icons = {
-    default = '',
-    symlink = '',
-    git = {
-       unstaged = "✗",
-       staged = "✓",
-       unmerged = "",
-       renamed = "➜",
-       untracked = "★",
-       deleted = "",
-       ignored = "◌"
-    },
-    folder = {
-       arrow_open = "",
-       arrow_closed = "",
-       default = "",
-       open = "",
-       empty = "",
-       empty_open = "",
-       symlink = "",
-       symlink_open = "",
-    },
-    lsp = {
-       hint = "",
-       info = "",
-       warning = "",
-       error = "",
+    -- disables netrw completely
+    disable_netrw       = true,
+    view = {
+        -- width of the window, can be either a number (columns) or a string in `%`
+        width = 40,
+        side = 'right',
+        mappings = {
+            -- custom only false will merge the list with the default mappings
+            -- if true, it will only use your list to set the mappings
+            custom_only = false,
+            -- list of mappings to set on the tree manually
+            list = {
+                { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
+                { key = {"<2-RightMouse>", "c"},        cb = tree_cb("cd") },
+                { key = "v",                            cb = tree_cb("vsplit") },
+                { key = "V",                            cb = tree_cb("split") },
+                { key = "t",                            cb = tree_cb("tabnew") },
+                { key = "<",                            cb = tree_cb("prev_sibling") },
+                { key = ">",                            cb = tree_cb("next_sibling") },
+                { key = "P",                            cb = tree_cb("parent_node") },
+                { key = "<BS>",                         cb = tree_cb("close_node") },
+                { key = "<S-CR>",                       cb = tree_cb("close_node") },
+                { key = "<Tab>",                        cb = tree_cb("preview") },
+                { key = "K",                            cb = tree_cb("first_sibling") },
+                { key = "J",                            cb = tree_cb("last_sibling") },
+                { key = "I",                            cb = tree_cb("toggle_ignored") },
+                { key = "H",                            cb = tree_cb("toggle_dotfiles") },
+                { key = "R",                            cb = tree_cb("refresh") },
+                { key = "a",                            cb = tree_cb("create") },
+                { key = "d",                            cb = tree_cb("remove") },
+                { key = "r",                            cb = tree_cb("rename") },
+                { key = "<C-r>",                        cb = tree_cb("full_rename") },
+                { key = "x",                            cb = tree_cb("cut") },
+                { key = "yy",                           cb = tree_cb("copy") },
+                { key = "p",                            cb = tree_cb("paste") },
+                { key = "y",                            cb = tree_cb("copy_name") },
+                { key = "Y",                            cb = tree_cb("copy_path") },
+                { key = "gy",                           cb = tree_cb("copy_absolute_path") },
+                { key = "[c",                           cb = tree_cb("prev_git_item") },
+                { key = "]c",                           cb = tree_cb("next_git_item") },
+                { key = "b",                            cb = tree_cb("dir_up") },
+                { key = "s",                            cb = tree_cb("system_open") },
+                { key = "q",                            cb = tree_cb("close") },
+                { key = "g?",                           cb = tree_cb("toggle_help") },
+            }
+        }
     }
 }
