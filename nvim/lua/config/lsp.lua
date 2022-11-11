70490ce13b25e9aca2b2f7d770514e99a1268924
Lee Ka Shing <leekashing2107@gmail.com>
2022-11-11 12:22:16 +0800

    call goimport on save

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,6 +1,7 @@
 local lsp = require('lspconfig')
 local remap = vim.api.nvim_set_keymap
 
+-- autocomplete
 vim.g.coq_settings = {
     auto_start = 'shut-up',
     keymap = { recommended = false }
@@ -13,14 +14,17 @@ remap('i', '<c-c>',   [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, 
 remap('i', '<tab>',   [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
 remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
 
+-- python lsp
 lsp.pylsp.setup{
     coq.lsp_ensure_capabilities()
 }
 
+-- clang lsp
 lsp.clangd.setup{
     coq.lsp_ensure_capabilities()
 }
 
+-- rust lsp
 require('rust-tools').setup({
     tools = {
         runnables = {
@@ -46,6 +50,15 @@ require('rust-tools').setup({
     }
 })
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.rs",
+    callback = function()
+        -- auto format rust code on save
+        vim.lsp.buf.format()
+    end,
+})
+
+-- go lsp
 lsp.gopls.setup {
     cmd = {"gopls"},
     settings = {
@@ -68,6 +81,7 @@ lsp.gopls.setup {
     coq.lsp_ensure_capabilities()
 }
 
+-- diagnostic float config
 vim.diagnostic.config({
     virtual_text = true,
     signs = false,
@@ -90,16 +104,25 @@ vim.diagnostic.config({
 --     end,
 -- })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.rs",
-    callback = function()
-       vim.lsp.buf.format()
-    end,
-})
+function go_org_imports(wait_ms)
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for cid, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                vim.lsp.util.apply_workspace_edit(r.edit, enc)
+            end
+        end
+    end
+end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*.go",
     callback = function()
-        require("go.format").gofmt()
+        -- auto format go code and organize imports on save
+        vim.lsp.buf.format()
+        go_org_imports(1000)
     end,
 })
