f9a9bb19900bf14af5bbcfaa03edf0ab4afe8bb7
Lee Ka Shing <leekashing2107@gmail.com>
2023-03-06 11:04:32 +0800

    chores

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,8 @@
--- local function prequire(m)
---     local ok, err = pcall(require, m)
---     if not ok then return nil, err end
---     return err
--- end
+-- load plugins
+require('plugins')
 
 -- load default config
 require('settings')
-
--- load plugins
-require('plugins')
 
 -- load local.lua
 require('local')
